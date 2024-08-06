86d9be2ad56f3191198566c87e151ecb6d7306a7
rachitshukla08 <rachitshukla08@gmail.com>
2024-08-06 22:45:38 +0530

    added middleware for error handling

--- api/index.js
+++ api/index.js
@@ -19,3 +19,13 @@ app.listen(3000, () => {
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const errorMessage = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    errorMessage,
+  });
+});
