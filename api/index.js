ba774bfbf298dc808b0363684792b29d7663830b
rachitshukla08 <rachitshukla08@gmail.com>
2024-08-06 20:54:23 +0530

    mongo setup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("mongodb connection established"))
+  .catch((e) => console.log(e));
 
 const app = express();
 app.listen(3000, () => {
