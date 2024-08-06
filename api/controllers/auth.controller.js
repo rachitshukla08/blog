d9bd3e7832c13ec766ca7d4df2931a138cda8ca0
rachitshukla08 <rachitshukla08@gmail.com>
2024-08-06 22:11:01 +0530

    added auth api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  console.log(req.body);
+  const { username, email, password } = req.body;
+  if (!username || !email || !password)
+    res.status(400).json({ message: "All fields are required" });
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,4 +16,6 @@ app.listen(3000, () => {
   console.log("listening on: 3000");
 });
 
+app.use(express.json());
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
