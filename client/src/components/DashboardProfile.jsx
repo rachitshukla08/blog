9fd7c6ef945ac813a0373627010d750139c00ff8
rachitshukla08 <rachitshukla08@gmail.com>
2024-08-18 16:44:13 +0530

    Added image upload functionality to firebase storage

--- client/src/components/DashboardProfile.jsx
+++ client/src/components/DashboardProfile.jsx
@@ -1,20 +1,115 @@
-import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const DashboardProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState();
+  const [imageFileUrl, setImageFileUrl] = useState();
+  const filePickerRef = useRef();
+  const [imgFileUploadProgress, setImageFileUploadProgress] = useState();
+  const [imgFileUploadError, setImageFileUploadError] = useState();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadProgress(null);
+        setImageFileUploadError(
+          "Couldnt upload, only images are allowed (Max file size is 2MB) "
+        );
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div className="w-32 relative h-32 self-center">
+          {imgFileUploadProgress && (
+            <CircularProgressbar
+              value={imgFileUploadProgress || 0}
+              text={`${imgFileUploadProgress}%`}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  userSelect: "none",
+                  pointerEvents: "none",
+                },
+                path: {
+                  stroke: `rgba(62,152,199,${imgFileUploadProgress / 100}`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="w-full h-full cursor-pointer rounded-full border-4 object-cover border-gray-200 dark:border-white"
+            onClick={() => {
+              filePickerRef.current.click();
+            }}
+            className={`w-full h-full cursor-pointer rounded-full border-4 object-cover border-gray-200 dark:border-white ${
+              imgFileUploadProgress && imgFileUploadProgress < 100
+                ? "opacity-50"
+                : "opacity-100"
+            } `}
           />
         </div>
+        {imgFileUploadError && (
+          <Alert color="failure">{imgFileUploadError}</Alert>
+        )}
         <TextInput
           type="text"
           id="username"
